a6b9200e7fe38c95dc42b506c0b47f564bcd3df3
ANILKUMARRegidi <129140166+ANILKUMARRegidi@users.noreply.github.com>
2023-12-18 11:26:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //your JS code here. If required.
 const currentURL = location.href;
 const count = currentURL.length;
-alert("The length of the URL is : " + count);
+alert("The length of the URL is: " + count);
